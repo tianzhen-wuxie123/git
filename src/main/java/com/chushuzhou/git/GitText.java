d7dc5aaf8f1f0283966b23866738050bbd057589
chushuzhou <787983017@qq.com>
2022-12-25 18:31:21 +0800

    secord

--- src/main/java/com/chushuzhou/git/GitText.java
+++ src/main/java/com/chushuzhou/git/GitText.java
@@ -3,5 +3,6 @@ package com.chushuzhou.git;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
