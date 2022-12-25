59722e0d53fd19d290a4bd638ed4ecb5e81c552e
chushuzhou <787983017@qq.com>
2022-12-25 18:32:18 +0800

    three

--- src/main/java/com/chushuzhou/git/GitText.java
+++ src/main/java/com/chushuzhou/git/GitText.java
@@ -4,5 +4,6 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
